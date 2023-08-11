dd14e32e5e784235d9b8b7c9811506883c6c0c0f
Bruno Ferreira <brunomgferreira17@gmail.com>
2023-08-11 12:24:35 +0100

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './styles/main.css';
+
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, now imported by this script
+    element.textContent = 'Hello webpack';
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
